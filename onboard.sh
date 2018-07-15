d802a0a21bea6708f769aca6dc7710e592f454b6
Travis Webb <me@traviswebb.com>
2018-07-15 19:41:33 -0400

    only download zip files

--- onboard.sh
+++ onboard.sh
@@ -9,19 +9,21 @@ while IFS=$CR read -r url
 do
   filename=$(basename "$url")
 
+  if [[ ! $filename =~ \.zip$ ]]; then continue; fi
+
   echo "--> loading $filename ..."
   wget $url
-  unzip -d $filename.dir $filename
+  mkdir $filename-dir
+  unzip -d $filename-dir $filename
 
-  cd $filename.dir
+  cd $filename-dir
   for las in *.las; do
     las2ogr -i $las -o lpc.shp
-    shp2pgsql -a -s 26918:4326 lpc public.lpc -f lpc.sql -W "latin1" -S
-    mv lpc.sql ../lpc-$filename.sql
+    shp2pgsql -a -s 26918:4326 lpc public.lpc -W "latin1" > ../lpc-$filename.sql
   done
 
   cd ..
-  rm $filename*
+  rm -rf $filename*
 
   echo "finished $filename"
 
