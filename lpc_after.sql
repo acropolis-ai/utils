fcf1d4c8b00362d3c4ceaa85c76af665ead550d6
Travis Webb <me@traviswebb.com>
2018-07-21 12:17:33 -0400

    convert elev to cm

--- lpc_after.sql
+++ lpc_after.sql
@@ -1,15 +1,18 @@
+drop table if exists public.lpc;
 create table public.lpc (
-  gid serial,
-  elevation double precision
+  elevation integer
 );
-select AddGeometryColumn('public', 'lpc', 'geom', 4326, 'POINT', 2);
+select AddGeometryColumn('public', 'lpc', 'geom', 3857, 'POINT', 2);
 
-select 
-  gid,
-  st_transform(st_setsrid(st_makepoint(st_x(geom), st_y(geom)), 26918), 4326) as geom,
-  st_z(geom) as elevation
-into public.lpc from onboard.lpc;
+insert into public.lpc (elevation, geom)
+select
+  (st_z(geom) * 100) as elev_cm,
+  st_transform(st_setsrid(st_makepoint(st_x(geom), st_y(geom)), 26918), 3857) as geom
+from onboard.lpc;
 
 create index lpc_geom_index on public.lpc using gist (geom);
+vacuum public.lpc;
+cluster public.lpc using lpc_geom_index;
+analyze public.lpc;
 
-drop table onboard.lpc;
+--drop table onboard.lpc;
